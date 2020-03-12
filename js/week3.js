9f8113ac2afa406b567757eda522dba079f65cb8
candice-jones <61527217+candice-jones@users.noreply.github.com>
2020-03-12 15:17:12 +1000

    Week 3

---
+++ js/week3.js
@@ -0,0 +1,73 @@
+//console.log('hello');
+
+//variable
+/*var sjkhfskhf
+kjsng kjshg;hskjgh;skdjh;nsjkgbrhsjgb;sg*/
+
+
+
+//primative data types, string, number, boolean, null, underfined
+
+//const firstName = "joe";
+//const lastName = "smith";
+//const age = 20;
+//const marriage = true;
+
+//console.log(`my name is ${firstName} ${lastName}. I'm ${age} years old.`);
+//console.log(firstName.toUpperCase());
+//firstName, lastName, age, gender, address: unit, street, sub, city, state, postcode
+
+//const person = {
+    // firstName:"joe"
+    // lastName:"smith"
+    // age: 20,
+    // marriage: true,
+    // address: {
+    //     unit:6,
+    //     street: "selbourne",
+    //     state:
+    //     city:
+    //     country:
+    // }
+
+   // console.log(person.firstName);
+
+    const person = {
+        firstName:"Candy",
+        lastName:"Jones",
+        age: 27,
+        gender: "female",
+        hobbies: "cleaning, ironing, mowing",
+        marriage: true,
+        address: {
+            unit:5,
+            street: "Ashby",
+            suburb:"Sippy Downs",
+            country:"Australia",
+            state: "Qld",
+            postcode: 4556,
+        }
+        };
+    
+      //  console.log ('Hey Im candy and im trying to learn Javascript');
+
+console.log(`my name is ${person.firstName} ${person.lastName}. I'm ${person.age} years old. I am a ${person.gender} I live at ${person.address.street} ${person.address.suburb} ${person.address.state}`);
+
+//console.log(person.hobbies[0]);
+//const hobbies ={folding, cleaning, weeding}
+
+//if
+
+console.log(person.firstName);
+console.log(person.hobbies[0]);
+console.log(person.firstName.length)
+
+//if
+
+if.person.age 
+let score = 50;
+if (score >50) {
+    console.log("pass!");
+} else if (score >75 && score<95) {
+    console.log ("see you next year!");
+}
